a3cd5eadda62c1b54a02da6577779aa79e656a24
Metodija Naumov <mnaumov@hotmail.com>
2025-09-09 03:59:19 +0200

    signUp component validation fix

--- UI/src/app/components/signup/signup.component.ts
+++ UI/src/app/components/signup/signup.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
-@Component({ selector: 'app-signup', templateUrl: 'signup.component.html' , imports : [ReactiveFormsModule, CommonModule]})
+@Component({ selector: 'app-signup', templateUrl: 'signup.component.html' , imports : [ReactiveFormsModule, CommonModule, RouterModule]})
 export class SignupComponent {
   form : FormGroup;
   error = '';
